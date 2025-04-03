05d850f735b11f8aca5764873db92de2eeb17df4
Logan Walker <loganaw@umich.edu>
2025-04-02 20:11:10 -0400

    Update DownloadFromFTP.py

--- other_scripts/DownloadFromFTP.py
+++ other_scripts/DownloadFromFTP.py
@@ -1,24 +1,33 @@
 from ftplib import FTP
 import os
+import tqdm
 
 # connect to NCBI FTP server
 ftp = FTP('ftp.ncbi.nlm.nih.gov')
 ftp.login()     # anonymous login
 
 # move to refseq directory
-ftp.cwd('refseq')
+#ftp.cwd('refseq')
 
 # list contents
-ftp.retrlines('LIST')
+#ftp.retrlines('LIST')
 
-# move to species specific subdirectory
-ftp.cwd('H_sapiens/mRNA_Prot')
-allfiles = ftp.nlst()
+for organism in ['H_sapiens', 'M_musculus']:
+    print(f'Downloading for {organism}...')
+    
+    # move to species specific subdirectory
+    ftp.cwd(f'/refseq/{organism}/mRNA_Prot')
+    allfiles = ftp.nlst()
 
-for file in allfiles:
-    if 'rna.fna' in file:
-        ftp.retrbinary('RETR ' + file,
-                       open(os.path.join(r'F:\ProbeDesign\RefSeqDatabase', file), 'wb').write)
+    allfiles = [x for x in allfiles if 'rna.fna' in x]
+
+    for file in tqdm.tqdm(allfiles):
+        #if 'rna.fna' in file:
+        #ftp.retrbinary('RETR ' + file,
+        #               open(os.path.join('F:\ProbeDesign\RefSeqDatabase', file), 'wb').write)
         print(file)
+
+        with open(f'./{file}', 'wb') as f:
+            ftp.retrbinary(f'RETR {file}', f.write)
 
 ftp.quit()
