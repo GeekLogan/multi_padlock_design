178a717abd4e501cc8d58441bb304d2a783a668e
Logan Walker <loganaw@umich.edu>
2025-04-02 20:07:05 -0400

    Update config.py

--- config.py
+++ config.py
@@ -5,11 +5,11 @@ max_MSA_processes = 6
 max_Blast_processes = 6
 
 # mouse database
-fastadir_mouse = r'C:\Worky\DB\RefSeq'
-fasta_filenum_mouse = 2
+fastadir_mouse = '/refseq/'
+fasta_filenum_mouse = 3
 fasta_pre_suffix_mouose = ('mouse.', '.rna.fna')
 
 # human database
-fastadir_human = r'C:\Worky\DB\RefSeq'
-fasta_filenum_human = 7
+fastadir_human = '/refseq/'
+fasta_filenum_human = 16
 fasta_pre_suffix_human = ('human.', '.rna.fna')
