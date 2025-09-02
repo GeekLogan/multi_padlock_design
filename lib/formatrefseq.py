8cccbe31c1888a75f36cea20f326d687f5481725
Logan Walker <loganaw@umich.edu>
2025-09-02 13:26:57 -0400

    Update formatrefseq.py

--- lib/formatrefseq.py
+++ lib/formatrefseq.py
@@ -16,22 +16,8 @@ def fastadb(indir, filenum, filename, name):
     for i in range(filenum):
         digit = str(i + 1)
 
-        if filename[1][-2:] == "gz":
-            # read zipped files directly
-            f = gzip.open(os.path.join(indir, filename[0] + digit + filename[1]), "rb")
-            fcontent = f.read()
-            f.close()
-
-            # write decompressed file
-            f2 = open(os.path.join(indir, filename[0] + digit + filename[1][:-2]), "wb")
-            f2.write(fcontent)
-            f2.close()
-
-            # delete compressed file
-            os.remove(os.path.join(indir, filename[0] + digit + filename[1]))
-
         with open(
-            os.path.join(indir, filename[0] + digit + filename[1].replace(".gz", "")),
+            os.path.join(indir, filename[0] + digit + filename[1]),
             "r",
         ) as f:
             print(digit)
@@ -40,14 +26,14 @@ def fastadb(indir, filenum, filename, name):
                 line = line.strip()
                 if line:
                     if line[0] == ">":  # a new sequence
-                        Seq.append([seq])
+                        if len(Headers):  # not the first sequence
+                            Seq.append([seq])
                         Headers.append(line)
                         seq = str()
                     else:
                         seq += line
 
     Seq.append(seq)  # the last sequence
-    del Seq[0]  # delete first empty element due to appending only
 
     # write all entries to file
     with open(os.path.join(indir, name + ".allheaders.txt"), "w") as f:
@@ -104,14 +90,16 @@ def fastadb(indir, filenum, filename, name):
 
 def blastdb(species):
     """Format fasta sequences to BLAST database"""
-    fastadir = (config.fastadir_mouse, config.fastadir_human)
-    nfiles = (config.fasta_filenum_mouse, config.fasta_filenum_human)
     filename = (config.fasta_pre_suffix_mouose, config.fasta_pre_suffix_human)
+    fastadir = (config.fastadir_mouse, config.fastadir_human, config.fastadir_fly)
+    nfiles = (config.fasta_filenum_mouse, config.fasta_filenum_human, config.fasta_filenum_fly)
 
     if species == "mouse":
         s = 0
     elif species == "human":
         s = 1
+    elif species == "fly":
+        s = 2
 
     if not os.path.isfile(os.path.join(fastadir[s], species + ".transcriptome.nal")):
         try:
